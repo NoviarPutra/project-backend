9456037902e8eee7c05e9926f1c34c656dc14ed8
itzfatoni <fatoni.hello@gmail.com>
2024-10-30 10:24:53 +0700

    differentiated user and public access

--- src/main/java/com/project/bootcamp_project/controller/DepartmentController.java
+++ src/main/java/com/project/bootcamp_project/controller/DepartmentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/department")
+@RequestMapping("/api/public/department")
 public class DepartmentController {
 
     @Autowired
