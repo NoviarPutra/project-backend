7fd3779f0f12ad6c3a90b880b3bee3571a7bb41a
itzfatoni <fatoni.hello@gmail.com>
2024-10-30 10:30:00 +0700

    differentiated user and public access

--- src/main/java/com/project/bootcamp_project/controller/JobPositionController.java
+++ src/main/java/com/project/bootcamp_project/controller/JobPositionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/job-position")
+@RequestMapping("/api/public/job-position")
 public class JobPositionController {
 
     @Autowired
